2add04d0fbce157813cb7303b1baecad61b96f3d
akshaykulkarni17 <akshaykulkarni17@gmail.com>
2021-11-11 22:10:16 +0530

    Initial commit

---
+++ src/main/java/com/sample/Server.java
@@ -0,0 +1,58 @@
+package com.sample;
+
+public class Server {
+
+    public String name;
+    public int processors;
+    public int memory;
+    public int diskspace;
+    public boolean isValid=true;
+
+    public Server(String name, int processors, int memory, int diskspace) {
+        this.name = name;
+        this.processors = processors;
+        this.memory = memory;
+        this.diskspace = diskspace;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProcessors() {
+        return processors;
+    }
+
+    public void setProcessors(int processors) {
+        this.processors = processors;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public int getDiskspace() {
+        return diskspace;
+    }
+
+    public void setDiskspace(int diskspace) {
+        this.diskspace = diskspace;
+    }
+
+    public boolean isValid() {
+        return isValid;
+    }
+
+    public void setValid(boolean valid) {
+        isValid = valid;
+    }
+// Getter/Setters method omitted for brevity
+}
